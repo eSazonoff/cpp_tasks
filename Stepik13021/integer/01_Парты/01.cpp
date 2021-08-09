38c2dbae3e543ee07f6a0a2e0d0b98f16ace53e2
Nikita Klok <35879564+eSazonoff@users.noreply.github.com>
2021-08-09 20:32:15 +0300

    All is correct.

--- Stepik13021/integer/01_Парты/01.cpp
+++ Stepik13021/integer/01_Парты/01.cpp
@@ -4,6 +4,6 @@ using namespace std;
 int main() {
     int c1, c2, c3;
     cin >> c1 >> c2 >> c3;
-    cout << (c1 + c2 + c3) / 2 + (c1 + c2 + c3) % 2;
+    cout << c1 / 2 + c2 / 2 + c3 / 2 + c1 % 2 + c2 % 2 + c3 % 2;
     return 0;
 }
