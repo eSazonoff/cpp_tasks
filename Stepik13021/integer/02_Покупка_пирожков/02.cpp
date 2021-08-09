dd7a5e0e267509aa888371c7450df0c6e5a81eac
Nikita Klok <35879564+eSazonoff@users.noreply.github.com>
2021-08-09 20:46:30 +0300

    Absolutely right.

--- Stepik13021/integer/02_Покупка_пирожков/02.cpp
+++ Stepik13021/integer/02_Покупка_пирожков/02.cpp
@@ -4,8 +4,10 @@ using namespace std;
 int main() {
     int A, B, N;
     cin >> A >> B >> N;
+    A *= N;
+    B *= N;
     A += B / 100;
     B %= 100;
-    cout << N * A << " " <<N * B;
+    cout << A << " " << B;
     return 0;
 }
