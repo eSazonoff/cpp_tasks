a0bfdb33271816bce194ba3855d181cf0d58eee3
Nikita Klok <35879564+eSazonoff@users.noreply.github.com>
2021-08-11 11:16:07 +0300

    Failed test #2 of 27. Wrong answer

--- Stepik13021/integer/03_Электронные_часы/03.cpp
+++ Stepik13021/integer/03_Электронные_часы/03.cpp
@@ -2,11 +2,15 @@
 using namespace std;
 
 int main() {
-    int h, m, n;
+    int h, m1, m2, s, n;
     cin >> n;
     h = n / 3600;
-    m = n % 3600 / 60;
+    m2 = n % 3600 / 60;
+    m1 = m2 / 10;
+    m2 %= 10;
     n = n % 3600 % 60;
-    cout << h << ":" << m << ":" << n;
+    s = n / 10;
+    n %= 10;
+    cout << h << ":" << m1 << m2 << ":" << s << n;
     return 0;
 }
