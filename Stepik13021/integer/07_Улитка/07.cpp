4084983c658ccead8e05aaa0a300358a78a68f18
Nikita Klok <35879564+eSazonoff@users.noreply.github.com>
2021-08-18 20:02:25 +0300

    Update 07.cpp

--- Stepik13021/integer/07_Улитка/07.cpp
+++ Stepik13021/integer/07_Улитка/07.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main() {
     int H, A, B, D;
     cin >> H >> A >> B;
-    D = (H - A + (A - B) - 1) / (A - B);
+    D = (H - B - 1) / (A - B);
     cout << D + 1;
     return 0;
 }
