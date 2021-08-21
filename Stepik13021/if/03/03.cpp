7936c7172fe2c40ed9148318d1c4c00a6504b316
Nikita Klok <35879564+eSazonoff@users.noreply.github.com>
2021-08-21 09:16:50 +0300

    right

---
+++ Stepik13021/if/03/03.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int A, B, C;
+  cin >> A >> B >> C;
+  if (A >= B) {
+      if (A >= C) cout << A;
+      else cout << C;
+  }
+  else if (B >= C) cout << B;
+       else cout << C;
+  return 0;
+}
