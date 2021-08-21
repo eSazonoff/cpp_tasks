14050209e34cb747b7cc15b1165d897c0d84f45a
Nikita Klok <35879564+eSazonoff@users.noreply.github.com>
2021-08-21 12:00:41 +0300

    Create 06.cpp

---
+++ Stepik13021/if/06/06.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int A1, A2, B1, B2;
+    cin >> A1 >> A2 >> B1 >> B2;
+    if (A1 == B1 || A2 == B2) cout << "YES";
+    else cout << "NO";
+  return 0;
+}
