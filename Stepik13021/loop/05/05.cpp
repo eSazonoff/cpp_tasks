b098548afac159d5236895cca633f17ea972ca17
Nikita Klok <35879564+eSazonoff@users.noreply.github.com>
2021-11-05 11:08:27 +0300

    Create 05.cpp

---
+++ Stepik13021/loop/05/05.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n, k = 0;
+    cin >> n;
+    while (n != 0) {
+        k++;
+        cin >> n;
+    }
+    cout << k;
+  return 0;
+}
